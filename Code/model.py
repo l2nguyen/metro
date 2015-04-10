ff66936f6c18320f99162322abe4dd8ce9a0aec6
Lena Nguyen <lena.nguyen4@gmail.com>
2015-04-10 17:30:20 -0400

    Added a second model with the addition of unemployment rate as a feature

--- Code/model.py
+++ Code/model.py
@@ -5,7 +5,7 @@
 # I'm interested in looking at the factors that affect
 # Metro rail ridership and modeling their relationships
 #===========================================================#
-# CREATED BY: Lena Nguyen - April 7, 2015
+# CREATED BY: Lena Nguyen - April 4, 2015
 #===========================================================#
 
 import pandas as pd
@@ -20,17 +20,18 @@ from sklearn.cross_validation import cross_val_score
 # IMPORT DATA #
 #=============#
 
-data = read.csv('../Data/model_data.csv')
+data = pd.read_csv('../Data/model_data.csv')
 
-#=======================================================#
-# LINEAR REGRESSION MODEL (MODEL #1)
-#=======================================================#
 # Split data into weekday and weekends (Temporary)
 # ! Might try standardizing the data based on number of cars later
 weekday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
 
 wkday = data[data.Weekday.isin(weekday)]
 wkend = data[~(data.Weekday.isin(weekday))]
+
+#=======================================================#
+# LINEAR REGRESSION MODEL (MODEL #1)
+#=======================================================#
 
 #-- 1. Run very simple model for weekday ridership with precipitation/snowfall variables
 #-- FEATURES: Precipitation, Snow depths, Snow fall amount
@@ -51,7 +52,6 @@ plt.xlabel("Snow Depths (inches)")
 plt.subplot(133)
 plt.scatter(wkday.SNOW, wkday.Riders, color='r', alpha=0.8)  # Plot the raw data
 plt.xlabel("Snowfall (inches)")
-
 
 feats = ['PRCP','SNWD','SNOW']
 
@@ -84,8 +84,11 @@ assert not np.any(np.isnan(X_test)|np.isinf(X_test))  # Just to be safe
 preds = plm.predict(X_test)
 np.sqrt(mean_squared_error(Y_test,preds))
 # That is a pretty bad mean square error even for the scale we are working with
+# RMSE = 105196.11853383623
 
 # Plot the residuals across the range of predicted values
+preds = np.ravel(preds)  # flatten ndarray
+
 resid = preds - Y_test['Riders']
 plt.scatter(preds, resid, alpha=0.7)
 plt.xlabel("Predicted Ridership")
@@ -96,6 +99,7 @@ plt.ylabel("Residuals")
 # Evaluate the model fit based off of cross validation
 scores = cross_val_score(plm, X, Y, cv=10, scoring='mean_squared_error')
 np.mean(np.sqrt(-scores))
+# 113921.50307552781
 # RMSE is about equivalent to the standard deviation of weekday ridership
 
 #-- CONCLUSION: Not a very good model at all. Maybe because there is not much
@@ -105,6 +109,57 @@ np.mean(np.sqrt(-scores))
 # LINEAR REGRESSION MODEL (MODEL #2)
 #=======================================================#
 
+#-- 2. Add unemployment rate as a feature 
+#-- FEATURES: Precipitation, Snow depths, Snow fall amount
+
+feats = ['PRCP','SNWD','SNOW', 'Unemp_Rate']
+
+X = wkday[feats]
+Y = wkday['Riders']
+
+# Split into train and test datasets
+# Test is 30% of complete dataset
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1)
+
+# Convert test/train data to data fame
+X_train = pd.DataFrame(data=X_train, columns=feats)
+X_test = pd.DataFrame(data=X_test, columns=feats)
+Y_train = pd.DataFrame(data=Y_train, columns=['Riders'])
+Y_test = pd.DataFrame(data=Y_test, columns=['Riders'])
+
+# Simple Linear Regression model
+plm = LinearRegression()
+plm.fit(X_train, Y_train)
+
+plm.intercept_
+plm.coef_
+
+#============#
+# MODEL EVAL #
+#============#
+
+# Evaluate the fit of the model based off of the training set
+assert not np.any(np.isnan(X_test)|np.isinf(X_test))  # Just to be safe
+preds = plm.predict(X_test)
+np.sqrt(mean_squared_error(Y_test,preds))
+# RMSE = 105110.70807714635
+# Mildly better than the previous model but still pretty bad
+
+# Plot the residuals across the range of predicted values
+preds = np.ravel(preds)  # flatten ndarray
+
+resid = preds - Y_test['Riders']
+plt.scatter(preds, resid, alpha=0.7)
+plt.xlabel("Predicted Ridership")
+plt.ylabel("Residuals")
+# Looking at the graph, the residuals are better for most cases than the first model
+# But the cases that are off are way off so are driving the RMSE up
+
+# Evaluate the model fit based off of cross validation
+scores = cross_val_score(plm, X, Y, cv=10, scoring='mean_squared_error')
+np.mean(np.sqrt(-scores))
+# 113389.06012018192
+# Betetr than model 1 but not by much
 
 
 
