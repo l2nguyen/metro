06d5a722f1669c3f88c91bec0979da00de5c24e5
Lena Nguyen <lena.nguyen4@gmail.com>
2015-04-20 20:59:32 -0400

    Generalized the date time column processing into a function

--- Code/cabi.py
+++ Code/cabi.py
@@ -1,30 +1,39 @@
-# Bikeshare data
-
+# Bikeshare data processing
 import pandas as pd
 
-data = '/Users/Zelda/Data Science/GA/Project/Data/Capital Bikeshare/2014-Q3-Trips-History-Data.csv'
+# Define function to reuse to process date data
+# Note: Q4 in 2014 has a different date format. It's %Y-%m-%d instead
+def process_date(df,var):
+    df[var] = pd.to_datetime(df[var], format='%m/%d/%Y %H:%M')
+    df.set_index(var, inplace=True)
+    df.sort_index(inplace=True)
+    # Separate date into separate variables for month, year, day
+    df['Year'] = df.index.year  # Create year variable
+    df['Month'] = df.index.month  # Create month variable
+    df['Day'] = df.index.day  # Create day variable
 
-data = pd.read_csv(data)
+year = range(2010, 2015)
+quarter = ['Q1', 'Q2', 'Q3', 'Q4']
 
+# Make list of column names
+cols = ['Duration', 'Start_date', 'End_date', 'Start_station', 'End_station', 'Bike', 'Type']
+
+# ? Will turn all this into a loop later
+data = '/Users/Zelda/Data Science/GA/Project/Data/Archives/CaBi/2011-Q1-Trips-History-Data.csv'
+# read data into dataframe
+data = pd.read_csv(data, header=True, names=cols)
+
+# Quick look at data
 data.dtypes
-data.head(20)
+data.head(10)
+data.columns.values
 
-data['Start date'] = pd.to_datetime(data['Start date'], format="%m/%d/%Y %H:%M")
-data.set_index('Start date', inplace=True)
-data.sort_index(inplace=True)
-## !! Error: time data '2014-10-01 00:00' does not match format '%m/%d/%Y %H:%M'
+process_date(data,'Start_date')
 
-# data['Start time'] = pd.to_datetime(data['Start time'], format="%m/%d/%Y %H:%M")
-# data.set_index('Start time', inplace=True)
+data.head(10)  # check it worked
 
-data['Year'] = data.index.year
-data['Month'] = data.index.month
-data['Day'] = data.index.day
-
-# data.groupby('Month')['Member Type'].apply(pd.value_counts)
-# data.groupby('Month')['Type'].apply(pd.value_counts)
-# data.groupby('Month')['Bike Key'].apply(pd.value_counts)
-# data.groupby('Month')['Subscriber Type'].apply(pd.value_counts)
-grouped = data.groupby(['Month','Day'])['Subscription Type'].apply(pd.value_counts)
+grouped = data.groupby(['Month','Day'])['Type'].apply(pd.value_counts)
 ##?? How to get a new data frame with the a column each counting the number of
 ##  casual and registered rider per day
+
+grouped.to_csv('/Users/Zelda/Data Science/GA/Project/Data/Archives/CaBi/type.csv')
