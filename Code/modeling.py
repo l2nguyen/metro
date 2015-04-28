4e9bea4f3f657d6d2d738108e4e540d974fc1a91
Lena Nguyen <lena.nguyen4@gmail.com>
2015-04-27 23:07:03 -0400

    Added code to deal with outliers. Added code for the weekday/weekend double model

--- Code/modeling.py
+++ Code/modeling.py
@@ -15,7 +15,6 @@ import matplotlib.pylab as plt
 from sklearn.preprocessing import scale
 from sklearn.cross_validation import train_test_split
 from sklearn.feature_selection import f_regression
-from sklearn.feature_selection import RFE
 from sklearn.linear_model import LinearRegression
 from sklearn import ensemble
 from sklearn.metrics import mean_squared_error
@@ -30,104 +29,84 @@ os.chdir("/Users/Zelda/Data Science/GA/Project/Data")
 
 data = pd.read_csv('model_data.csv')
 
-# Split data into weekday and weekends (Temporary)
-weekday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
-
-wkday = data[data.Weekday.isin(weekday)]
-wkend = data[~(data.Weekday.isin(weekday))]
-
 # Alternatively, create ridership per car variable
 data['RidersPC'] = data['Riders']/data['Cars']
 data.head()  # Check it worked
-
-cols = data.columns.values
-feats = cols[5:-2]
-
-# ! Temporary
-data['Registered'].fillna(value=0, inplace=True)
-data['Casual'].fillna(value=0, inplace=True)
-
-X = data[feats]
-Y = data['RidersPC']
 
 #=======================================================#
 # DEAL WITH OUTLIERS
 #=======================================================#
 
+# Make z score for Riders Per Car data to flag outlier
+data['RidersPC_z'] = (data['RidersPC'] - data['RidersPC'].mean()) / data['RidersPC'].std()
+
+# Look at obs +/- 2.5 SD away
+data[(abs(data['RidersPC_z']) >= 2.5)]
+#- 53 observations out of 4018 (~1% of obs)
+#- The majority of outliers are on the minus side.
+#- Also, the majority of them are holidays.
+#- Will try making two different models for holiday and regular days
+
+# Trim outliers from dataset
+trim_data = data[(abs(data['RidersPC_z']) < 2.5)]
+
+# Identify feature variabbles
+feats = data.columns.values[5:-3]
+
+# Bikeshare did not exist before 2010 so will fill NaN values with 0
+# for all the other models
+trim_data['Registered'].fillna(value=0, inplace=True)
+trim_data['Casual'].fillna(value=0, inplace=True)
+trim_data.isnull().sum()  # check it worked
+
+X = trim_data[feats]
+Y = trim_data['RidersPC']
+
 #=======================================================#
-# STANDARDIZE DATA
+# FEATURE SELECTION (TRIMMED DATASET)
 #=======================================================#
 
-std_X = scale(X)  # Standardize features
-std_Y = scale(Y)  # Standardize response variable
 
-# Change into dataframe for easier visualizing
-std_X = pd.DataFrame(data=std_X, columns=feats)
-std_Y = pd.DataFrame(data=std_Y, columns=['RidersPC'])
+def feat_sel(feats, resp):  # Function to run feature selection
+    std_feats = scale(feats)  # Standardize features
+    std_resp = scale(resp)  # Standardize response variable
+    # Run f-test for feature selection
+    ftest = f_regression(std_feats, std_resp)
+    feat_sel = pd.DataFrame(data=ftest[0], index=feats.columns.values, columns=['F_score'])  # makes data frame with p values
+    feat_sel['p_values'] = ftest[1]  # Make p-values column
+    return feat_sel  # print out feature selection
+
+feat_sel(X,Y)  # Run feature selection on trimmed dataset
+#-- Let's put the threshold at F_score of 40.
+#-- From this, it appears the significant features are:
+#-- TMAX, TMIN, Gas_Price, Labor Force, Employment
+#-- Registered CaBi Riders, Casual CaBi Riders, Holiday
+
+# Use only feats that have an F-score over the threshold
+new_feats = ['TMAX', 'TMIN', 'Labor Force', 'Employment', 'Registered', 'Casual', 'Holiday']
+
+X = trim_data[new_feats]
+Y = trim_data['RidersPC']
 
 #=======================================================#
-# SPLIT DATASET
+# SPLIT INTO TRAIN/TEST (TRIMMED DATASET)
 #=======================================================#
 
-# Split into train and test datasets
-# Test is 30% of complete dataset
-# X_train, X_test, Y_train, Y_test = train_test_split(std_X, std_Y, test_size=0.3, random_state=1)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1)
 
-# Convert test/train data to data fame
-X_train = pd.DataFrame(data=X_train, columns=feats)
-X_test = pd.DataFrame(data=X_test, columns=feats)
+# Change into data frames for easier visualization
+X_train = pd.DataFrame(data=X_train, columns=X.columns.values)
+X_test = pd.DataFrame(data=X_test, columns=X.columns.values)
 Y_train = pd.DataFrame(data=Y_train, columns=['RidersPC'])
 Y_test = pd.DataFrame(data=Y_test, columns=['RidersPC'])
 
 #=======================================================#
-# FEATURE SELECTION
+# LINEAR REGRESSION MODEL (TRIMMED DATASET)
 #=======================================================#
 
-# Find features that are significant
-ftest = f_regression(X,Y)
-feat_sel = pd.DataFrame(data=ftest[0], index=feats, columns=['F_score'])  # makes data frame with p values
-feat_sel['p_values'] = ftest[1]  # I want to see the relationship between F-score and p values
-
-feat_sel
-#-- Let's put the threshold at F_score of 40.
-#-- From this, it appears the significant features are:
-#-- SNWD, TMAX, TMIN, Gas_Price, Labor Force, Employment
-#-- Registered Cabi Riders, Holiday
-
-# Try out recurse feature elimination
-lm = LinearRegression()
-rfe = RFE(lm, 8)  # selects top 8 features
-rfe = rfe.fit(X_train,Y_train)
-
-print(rfe.support_)
-print(rfe.ranking_)
-rfe.ranking_
-
-feat_sel['Ranking'] = rfe.ranking_
-
-feat_sel
-
-#=======================================================#
-# LINEAR REGRESSION MODEL
-#=======================================================#
-
-mfeats = ['SNWD', 'TMAX', 'TMIN', 'Labor Force', 'Employment', 'Registered', 'Casual', 'Holiday']
-
-m_X = std_X[mfeats]
-m_Y = std_Y['RidersPC']
-
-mX_train, mX_test, mY_train, mY_test = train_test_split(m_X, m_Y, test_size=0.3, random_state=1)
-
-# Convert test/train data to data fame
-mX_train = pd.DataFrame(data=mX_train, columns=mfeats)
-mX_test = pd.DataFrame(data=mX_test, columns=mfeats)
-mY_train = pd.DataFrame(data=mY_train, columns=['RidersPC'])
-mY_test = pd.DataFrame(data=mY_test, columns=['RidersPC'])
-
-# Simple Linear Regression model
+# Linear regression model
 plm = LinearRegression()
-plm.fit(mX_train, mY_train)
+plm.fit(X_train, Y_train)
 
 plm.intercept_
 plm.coef_
@@ -137,27 +116,27 @@ plm.coef_
 #============#
 
 # Evaluate the fit of the model based off of the training set
-assert not np.any(np.isnan(mX_test) | np.isinf(mX_test))  # Just to be safe
-preds = plm.predict(mX_test)
-np.sqrt(mean_squared_error(mY_test,preds))
-# RMSE = 0.9247
-# Not so good still.
+assert not np.any(np.isnan(X_test) | np.isinf(X_test))  # Just to be safe
+preds = plm.predict(X_test)
+np.sqrt(mean_squared_error(Y_test,preds))
+# RMSE = 1137.2
+# Not bad. It's about equivalent to the standard deviation of the Riders per car variable
 
 # Plot the residuals across the range of predicted values
 preds = np.ravel(preds)  # flatten ndarray
 
-resid = preds - mY_test['RidersPC']
+resid = preds - Y_test['RidersPC']
 plt.scatter(preds, resid, alpha=0.7)
 plt.xlabel("Predicted Ridership per Car")
 plt.ylabel("Residuals")
 plt.show()
-# Looking at the graph, the residuals are better for most cases than the first model
-# But the cases that are off are way off so are driving the RMSE up
+# Looking at the graph, it appears that the residuals fall more on the negative side
+# This model seems to lean towards predicting inaccurately on the lesser side than the higher side
 
 # Evaluate the model fit based off of cross validation
-scores = cross_val_score(plm, m_X, m_Y, cv=20, scoring='mean_squared_error')
+scores = cross_val_score(plm, X, Y, cv=20, scoring='mean_squared_error')
 np.mean(np.sqrt(-scores))
-# 0.924
+# RMSE from cross validation = 1175.67
 
 #=======================================================#
 # RANDOM FOREST MODEL
@@ -165,22 +144,178 @@ np.mean(np.sqrt(-scores))
 
 rtr = ensemble.RandomForestRegressor()
 rtr.fit(X_train,Y_train)
-rtr.feature_importances_
-
 rtr.score(X_train,Y_train)
 # R squared is 0.862 - OK
 
 rtr.score(X_test,Y_test)
 # really terrible - it does not appear this current model is generalizable
 
-feat_select = pd.DataFrame(data=rtr.feature_importances_, index=feats, columns=['importance'])
+rtr_select = pd.DataFrame(data=rtr.feature_importances_, index=X.columns.values, columns=['importance'])
 
 preds = rtr.predict(X)
 mean_squared_error(Y, preds)
-# 593895 - really terrible
+# 554945 - really terrible
 
 resid = preds - Y
 plt.scatter(preds, resid, alpha=0.7)
 plt.xlabel("Predicted Riders per Car")
 plt.ylabel("Residuals")
 plt.show()
+#-- The residuals are all over the place
+#-- Probably will not use random forest model
+
+##########################################################################
+
+#=======================================================#
+# DATASET SPLIT FOR WEEKDAY/WEEKEND MODELS
+#=======================================================#
+
+# NOTE: Using dataset where the outliers have been trimmed
+
+# WEEKDAY/WEEKEND SPLIT -------------------------------
+weekday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+
+# Feature dataset
+X_wkday = trim_data[feats][data.Weekday.isin(weekday)]
+X_wkend = trim_data[feats][~(data.Weekday.isin(weekday))]
+
+# Respnse dataset
+Y_wkday = trim_data['RidersPC'][data.Weekday.isin(weekday)]
+Y_wkend = trim_data['RidersPC'][~(data.Weekday.isin(weekday))]
+
+#=======================================================#
+# FEATURE SELECTION (WEEKDAY/WEEKEND)
+#=======================================================#
+
+# ? Wonder if weekday/weekends will have different feature importance
+
+feat_sel(X_wkday, Y_wkday)
+# About the same as the full dataset but CaBi seems to have no importance
+
+feat_sel(X_wkend, Y_wkend)
+# Employment variables less important. CaBi more important
+# Holiday has very little effect on weekend ridership
+
+# Weekday features (thresdhold: F-score>40)
+wkday_feats = ['SNWD', 'TMAX', 'TMIN', 'Gas_Price', 'Labor Force', 'Employment', 'Unemployment', 'Holiday']
+wkend_feats = ['TMAX', 'TMIN', 'Gas_Price', 'Labor Force', 'Employment', 'Casual']
+
+# Replace old datasets with new one with selected features
+X_wkday = X_wkday[wkday_feats]
+X_wkend = X_wkend[wkend_feats]
+
+#=======================================================#
+# SPLIT INTO TRAIN/TEST (TRIMMED DATASET)
+#=======================================================#
+
+# WEEKDAY DATASET
+Xwkday_train, Xwkday_test, Ywkday_train, Ywkday_test = train_test_split(X_wkday, Y_wkday, test_size=0.3, random_state=1)
+
+# WEEKEND DATASET
+Xwkend_train, Xwkend_test, Ywkend_train, Ywkend_test = train_test_split(X_wkend, Y_wkend, test_size=0.3, random_state=1)
+
+#=======================================================#
+# LINEAR REGRESSION MODEL (WEEKDAY/WEEKEND MODELS)
+#=======================================================#
+
+# WEEKDAY MODEL
+wlm = LinearRegression()
+wlm.fit(Xwkday_train, Ywkday_train)
+
+wlm.intercept_
+wlm.coef_
+
+# WEEKDAY MODEL
+wnlm = LinearRegression()
+wnlm.fit(Xwkend_train, Ywkend_train)
+
+wnlm.intercept_
+wnlm.coef_
+
+#=====================================#
+# MODEL EVAL (WEEKDAY/WEEKEND MODELS) #
+#=====================================#
+
+
+#===============#
+# WEEKDAY MODEL
+#===============#
+
+# Evaluate the fit of the model based off of the training set
+assert not np.any(np.isnan(Xwkday_test) | np.isinf(Xwkday_test))  # Just to be safe
+preds = wlm.predict(Xwkday_test)
+np.sqrt(mean_squared_error(Ywkday_test,preds))
+# RMSE = 648.1
+# Performs better than the model of the full dataset
+
+# Plot the residuals across the range of predicted values
+preds = np.ravel(preds)  # flatten ndarray
+
+resid = preds - Ywkday_test
+plt.scatter(preds, resid, alpha=0.7)
+plt.xlabel("Predicted Ridership per Car (Weekdays)")
+plt.ylabel("Residuals")
+plt.show()
+# Got most of the points right but some points are pretty off driving up the RMSE
+
+# Evaluate the model fit based off of cross validation
+scores = cross_val_score(plm, X_wkday, Y_wkday, cv=20, scoring='mean_squared_error')
+np.mean(np.sqrt(-scores))
+# RMSE from cross validation = 646.72
+
+#===============#
+# WEEKEND MODEL
+#===============#
+
+# Evaluate the fit of the model based off of the training set
+assert not np.any(np.isnan(Xwkend_test) | np.isinf(Xwkend_test))  # Just to be safe
+preds = wnlm.predict(Xwkend_test)
+np.sqrt(mean_squared_error(Ywkend_test,preds))
+# RMSE = 629.24
+# Much worse at predicting weekend ridership
+
+# Plot the residuals across the range of predicted values
+preds = np.ravel(preds)  # flatten ndarray
+
+resid = preds - Ywkend_test
+plt.scatter(preds, resid, alpha=0.7)
+plt.xlabel("Predicted Ridership per Car (Weekends)")
+plt.ylabel("Residuals")
+plt.show()
+# Got most of the points right but some points are pretty off driving up the RMSE
+
+# Evaluate the model fit based off of cross validation
+scores = cross_val_score(wnlm, X_wkend, Y_wkend, cv=20, scoring='mean_squared_error')
+np.mean(np.sqrt(-scores))
+# RMSE from cross validation = 687.03
+
+#-- This double model performs better than the full dataset model
+#-- However, the lower sample size for weekend will cause issues with how
+#-- generalizable this model will be
+
+
+
+
+# SPLIT OFF DATASET WHERE CAPITAL BIKESHARE EXISTED
+dcabi = data[data['Registered'].isnull() == False]
+dcabi.describe()
+
+# Make sure the min year is 2010 and max year is 2014
+assert min(dcabi.Year) == 2010
+assert max(dcabi.Year) == 2014
+dcabi.isnull().sum()
+
+X_cabi = dcabi[feats]
+Y_cabi = dcabi['RidersPC']
+
+
+
+
+# HOLIDAY/REGULAR DAY SPLIT ----------------------------
+# Feature dataset
+X_regular = data[feats][data['Holiday'] == 0]
+X_holiday = data[feats][data['Holiday'] == 1]
+
+# Response dataset
+Y_regular = data['RidersPC'][data['Holiday'] == 0]
+Y_holiday = data['RidersPC'][data['Holiday'] == 1]
